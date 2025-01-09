9b93f45ce96668bddcc5a8cfbf1893f8a9511854
nolanborz <nolanrborzoni@gmail.com>
2025-01-09 16:46:20 +0900

    Add some dynamic js into the content div

--- src/index.js
+++ src/index.js
@@ -2,3 +2,18 @@ import "./styles.css";
 import { greeting } from "./greeting.js";
 
 console.log(greeting);
+//First, select the element//
+const content = document.getElementById("content");
+//Create the elements you want to inject//
+const header = document.createElement("h1");
+const paragraph = document.createElement("p");
+//Add content to the elements//
+header.textContent = "Nona's Pasta Italiana";
+paragraph.textContent = "This pasta kitchen revolutionizes pasta as we know it, and the best part? It comes all from Nona's kitchen.";
+
+
+
+//Append the new elements to the target div//
+content.appendChild(header);
+content.appendChild(paragraph);
+
