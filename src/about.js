caac5c037003d7d766b49667c8e171d3b3691a04
nolanborz <nolanrborzoni@gmail.com>
2025-01-11 11:33:45 +0900

    Update some css and content

--- src/about.js
+++ src/about.js
@@ -5,7 +5,7 @@ export function loadAbout() {
     const story = document.createElement('p');
     
     header.textContent = 'About Us';
-    story.textContent = 'Founded in 1925, Nona\'s Pasta Italiana has been serving authentic Italian cuisine for generations. Our recipes have been passed down through our family, preserving the traditional flavors of Italy.';
+    story.innerHTML = "This smalltown pasta kitchen is ran by a 5th generation pasta famiglia. The head of it all is <strong>Nona</strong>, or grandma. This means one thing: <strong>Keep it simple and make it right</strong>. We take these words to heart with every sauce and every noodle we make. That's what keeps our customers coming back. Where else can you indulge in 400 year old recipes? Only here, at Nona's Pasta Italiana. Thanks for your relentless support, and <em>Buona Appetito</em>.";
     
     content.appendChild(header);
     content.appendChild(story);
--- src/home.js
+++ src/home.js
@@ -3,11 +3,17 @@ import forkIcon from './fork.png';
 export function loadHome() {
     const content = document.getElementById('content');
     const header = document.createElement('h1');
+    const menuUpdateTitle = document.createElement('h2')
+    const menuUpdateText = document.createElement('p');
     const paragraph = document.createElement('p');
     
     header.textContent = "Nona's Pasta Italiana";
-    paragraph.textContent = "This pasta kitchen revolutionizes pasta as we know it, and the best part? It comes all from Nona's kitchen.";
+    menuUpdateTitle.innerHTML = "Margharita Pizza is <strong>BACK</strong>";
+    menuUpdateText.innerHTML = "You spoke, and we listened. The Margharita Pizza is making a comeback on <strong>August 1st</strong>, put your orders in now!";
+    paragraph.innerHTML = "This smalltown pasta kitchen is ran by a 5th generation pasta famiglia. The head of it all is <strong>Nona</strong>, or grandma. This means one thing: <strong>Keep it simple and make it right</strong>. We take these words to heart with every sauce and every noodle we make. That's what keeps our customers coming back. Where else can you indulge in 400 year old recipes? Only here, at Nona's Pasta Italiana. Thanks for your relentless support, and <em>Buona Appetito</em>.";
     
     content.appendChild(header);
+    content.appendChild(menuUpdateTitle);
+    content.appendChild(menuUpdateText);
     content.appendChild(paragraph);
 }
